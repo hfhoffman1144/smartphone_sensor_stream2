4904c696b39b46d9b356fff5a32fb8ddc22044d5
hfhoffman1144 <hfh160130@utdallas.edu>
2023-03-21 09:49:00 -0500

    ksql table working

--- producer/app/main.py
+++ producer/app/main.py
@@ -6,6 +6,21 @@ from schemas.sensors import SensorReading, SensorResponse
 from core.config import app_config
 from loguru import logger
 
+
+# TODO: Move to separate file
+def flatten_dict(nested_dict, parent_key='', sep='_'):
+    """
+    This function takes in a nested dictionary and returns a flattened dictionary.
+    """
+    items = []
+    for key, value in nested_dict.items():
+        new_key = f"{parent_key}{sep}{key}" if parent_key else key
+        if isinstance(value, dict):
+            items.extend(flatten_dict(value, new_key, sep=sep).items())
+        else:
+            items.append((new_key, value))
+    return dict(items)
+
 app = FastAPI(title=app_config.PROJECT_NAME)
 
 loop = asyncio.get_event_loop()
@@ -42,7 +57,17 @@ async def kafka_produce(data: SensorReading):
         from the request.
     """
 
-    await producer.send(app_config.TOPIC_NAME, json.dumps(data.dict()).encode("ascii"))
+    # Extract the messageId, deviceId, and sessionId
+    message_info = data.dict().copy()
+    message_info.pop('payload')
+
+    # Write each individual sensor reading in the payload to kafka
+    for sensor_reading in data.dict()['payload']:
+
+        kafka_message = {**flatten_dict(sensor_reading), **message_info}
+        await producer.send(app_config.TOPIC_NAME, json.dumps(kafka_message).encode("ascii"))
+
+        print(kafka_message)
 
     response = SensorResponse(
         messageId=data.messageId,
